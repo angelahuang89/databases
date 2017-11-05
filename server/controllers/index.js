c929ef64639b88ce5501e89d432d5e161f02057f
Angela Huang <angelahuang89@gmail.com>
2017-11-05 12:54:07 -0800

    Write controller methods and alter spec tests

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -2,14 +2,50 @@ var models = require('../models');
 
 module.exports = {
   messages: {
-    get: function (req, res) {}, // a function which handles a get request for all messages
-    post: function (req, res) {} // a function which handles posting a message to the database
+    get: function (req, res) {
+      model.messages.get(function(error, results) {
+      	if (error) {
+      	  throw error;
+      	} else {
+      	  res.json(results);
+      	}
+      });
+    }, // a function which handles a get request for all messages
+    post: function (req, res) {
+      var message = [req.body.message, req.body.roomname, req.body.username];
+      model.messages.post(message, function(error, results) {
+        if (error) {
+          throw error;
+        } else {
+          res.sendStatus(201);
+          res.end();
+        }
+      });
+    } // a function which handles posting a message to the database
   },
 
   users: {
     // Ditto as above
-    get: function (req, res) {},
-    post: function (req, res) {}
+    get: function (req, res) {
+      models.users.get(function(error, results) {
+      	if (error) {
+          throw error;
+      	} else {
+      	  res.json(results);
+      	}
+      })
+    },
+    post: function (req, res) {
+      var user = [req.body.username];
+      models.users.post(user, function(error, results) {
+        if (error) {
+          throw error;
+        } else {
+          res.sendStatus(201);
+          res.end();
+        }
+      });
+    }
   }
 };
 
--- server/spec/server-spec.js
+++ server/spec/server-spec.js
@@ -10,8 +10,8 @@ describe('Persistent Node Chat Server', function() {
 
   beforeEach(function(done) {
     dbConnection = mysql.createConnection({
-      user: 'root',
-      password: '',
+      user: 'student', // root
+      password: 'student', // plantlife
       database: 'chat'
     });
     dbConnection.connect();
